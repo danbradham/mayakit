af0bee1c95b176ed312e3b95c306b8062b458417
Dan Bradham <danielbradham@gmail.com>
2016-11-21 19:05:17 -0500

    Added callbacks.py including a nifty MelProcCallback object allowing users to add python callbacks to global mel procedures

---
+++ mayakit/callbacks.py
@@ -0,0 +1,143 @@
+import re
+from collections import defaultdict
+from maya import mel, cmds
+
+
+class MelProcCallback(object):
+    '''
+    Maintains a registry of python callbacks for mel global procedures.
+    '''
+
+    _registry = defaultdict(list)
+    _hook_tmpl_no_params = ('python("from mayakit.callbacks import MelProcCallback; MelProcCallback.execute(\'{}\')");')
+    _hook_tmpl = ('python("from mayakit.callbacks import MelProcCallback; MelProcCallback.execute(\'{}\', \'" + {} + "\')");')
+    _proc_cache = {}
+
+    @classmethod
+    def add(cls, mel_proc, callback):
+        '''Add callback to mel procedure'''
+
+        if not mel_proc in cls._proc_cache:
+            try:
+                mel_file, mel_code, params = get_mel_proc_data(mel_proc)
+                cls._proc_cache[mel_proc] = mel_file, mel_code, params
+            except:
+                raise
+        else:
+            mel_file, mel_code, params = cls._proc_cache[mel_proc]
+
+        if params:
+            py_hook = cls._hook_tmpl.format(mel_proc, ' + "\', \'" + '.join(params))
+        else:
+            py_hook = cls._hook_tmpl_no_params.format(mel_proc)
+
+        if not py_hook in mel_code:
+            mel_code = mel_code[:-1] + py_hook + mel_code[-1]
+
+            try:
+                mel.eval(mel_code)
+            except Exception as e:
+                print 'Failed to add python callback hook'
+                print e
+                return
+
+        cls._registry[mel_proc].append(callback)
+
+    @classmethod
+    def remove(cls, mel_proc, callback):
+        '''Remove callback from mel procedure'''
+
+        try:
+            cls._registry[mel_proc].remove(callback)
+        except ValueError:
+            pass
+
+        if not cls._registry[mel_proc] and mel_proc in cls._proc_cache:
+            mel.eval(cls._proc_cache[mel_proc][1])
+
+    @classmethod
+    def clear(cls):
+        '''Clear all callbacks and source original mel files'''
+
+        for mel_proc, (_, mel_code, _) in cls._proc_cache.items():
+            mel.eval(mel_code)
+
+        cls._registry = defaultdict(list)
+
+    @classmethod
+    def execute(cls, mel_proc, *mel_proc_args):
+        '''Execute all callbacks for mel procedure'''
+
+        for callback in cls._registry[mel_proc]:
+            callback(*mel_proc_args)
+
+
+def get_mel_proc_data(mel_proc):
+    '''Get the mel code and parameters of global mel procedure'''
+
+    mel_file = mel.eval('whatIs ' + mel_proc)
+    if mel_file == 'Unknown':
+        raise Exception('Mel procedure is unknown')
+    elif 'found in: ' in mel_file:
+        mel_file = mel_file.split('found in: ')[-1]
+    else:
+        raise Exception('Mel procedure is not defined in a file, can not add callback')
+
+    with open(mel_file, 'r') as f:
+        file_contents = f.read()
+
+    mel_code = []
+
+    pattern = re.compile('global proc %s\(.*\)\s+?\{' % (mel_proc))
+    match = pattern.search(file_contents)
+    if not match:
+        raise Exception('Can not find global proc ' + mel_proc)
+
+    mel_def = match.group(0)
+    mel_def_end = match.end(0)
+    pattern = re.compile('\$\w+')
+    params = pattern.findall(mel_def)
+
+    mel_code.append(mel_def)
+    depth = 1
+    for char in file_contents[mel_def_end:]:
+        if depth == 0:
+            break
+
+        mel_code.append(char)
+        if char == '{':
+            depth += 1
+        elif char == '}':
+            depth -= 1
+
+    return mel_file, ''.join(mel_code), params
+
+
+def safe_add_menuItem(menu_path, menu_item_path, **kwargs):
+
+    cmds.setParent(menu_path, m=True)
+    if not cmds.menuItem(menu_item_path, exists=True):
+        cmds.menuItem(menu_item_path, **kwargs)
+
+    return menu_item_path
+
+
+def whatIs(mel_proc):
+    if mel_proc in MelProcCallback._registry:
+        print MelProcCallback._registry[mel_proc][0]
+    else:
+        mel.eval('whatIs {};'.format(mel_proc))
+
+
+if __name__ == '__main__':
+    def view_menu_callback(*args):
+        cmds.setParent(args[0], m=True)
+        menu_item = safe_add_menuItem(
+            args[0],
+            args[1] + 'hello',
+            label='HELLO',
+            checkBox=False,
+        )
+
+    MelProcCallback.add('postModelEditorViewMenuCmd', view_menu_callback)
+    MelProcCallback.remove('postModelEditorViewMenuCmd', view_menu_callback)
